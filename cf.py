1ee658a70edaca12264b340b65d774b422044bcf
Christopher Curl <christopher.r.curl@gmail.com>
2023-03-13 23:03:14 -0700

    final commit

--- cf.py
+++ cf.py
@@ -4,23 +4,26 @@ Calculations
 
 from __init__ import ureg, Q_
 from math import log, pi
-import numpy as np
 
 
 def calc_q_temp_oh(constants=None, props=None):
     mdot_c = constants["mdot_c"]
+    assert mdot_c.units == ureg.kg / ureg.seconds
     temp_oc = constants["temp_oc"]
     temp_ic = constants["temp_ic"]
     cp_c = props["cp_c"]
+    assert cp_c.units == ureg.joules / (ureg.kg * ureg.degK)
 
-    q = mdot_c * cp_c * (temp_oc - temp_ic)
+    q = (mdot_c * cp_c * (temp_oc - temp_ic)).to('watts')
 
     mdot_h = constants["mdot_h"]
-    T_ih = constants["T_ih"]
+    assert mdot_h.units == ureg.kg / ureg.seconds
+    temp_ih = constants["temp_ih"]
     cp_h = props["cp_h"]
+    assert cp_h.units == ureg.joules / (ureg.kg * ureg.degK)
 
-    T_oh = (q / (mdot_h * cp_h) - T_ih) * -1
-    return q, T_oh
+    temp_oh = ((q / (mdot_h * cp_h) - temp_ih) * -1).to('degK')
+    return q, temp_oh
 
 
 def calc_cr_cmin_cmax(constants=None, props=None):
@@ -66,7 +69,6 @@ Parametric calculations
 """
 
 
-# TODO: Do parametric calc in main.py
 def calc_hc(constants=None, props=None, tube_diameter_outer=None, tube_thickness=None):
     mdot_c = constants["mdot_c"]
     mu_c = props["mu_c"]
@@ -83,14 +85,123 @@ def calc_hc(constants=None, props=None, tube_diameter_outer=None, tube_thickness
         # Flow is laminar
         nu_d = 4.36     # Uniform heat flux
 
-    h_c = (nu_d * k_c)/inner_diam
+    h_c = ((nu_d * k_c)/inner_diam).to(ureg.watts / (ureg.meters**2 * ureg.degK))
     return h_c
 
 
-def calc_hh():
-    return None
+def calc_re_d_max(constants=None, props=None, tube_diameter_outer=None):
+    rho_h = props["rho_h"]
+    mu_h = props["mu_h"]
+    nu_h = mu_h / rho_h
+    velocity = constants['air_vel']
+    s_t = constants["s_t"]
+    s_d = constants["s_d"]
+
+    # Step 1 - calculate max velocity of air through tube bank
+    vel_max = s_t * velocity / (2 * (s_d - tube_diameter_outer))
+
+    # Step 2 - calculate reynolds number using vmax
+    re_d_max = vel_max * tube_diameter_outer / nu_h
+
+    return re_d_max
+
+
+def calc_nu_hh_20_plus_NL(constants=None, props=None, re_d_max=None, tube_diameter_outer=None):
+    # Use different c1 coefficients based on Re_D_max values
+    # Assumed staggered array
+
+    s_t = constants['s_t']
+    s_l = constants['s_l']
+    pr_h = props['pr_h']
+    pr_hs = props['pr_hs']
+    k_h = props['k_h']
+
+    if 10 <= re_d_max <= 10**2:
+        c1 = 0.9
+        m = 0.4
+        nu_d = c1 * re_d_max ** m * pr_h ** 0.36 * (pr_h / pr_hs) ** 0.25
+    elif 10**2 < re_d_max <= 10**3:
+        c1 = 0.683
+        m = 0.466
+        nu_d = c1 * re_d_max**m * pr_h**(1/3)
+    elif 10**3 < re_d_max <= 2 * 10**5 and s_t/s_l < 2:
+        c1 = 0.35 * (s_t/s_l)**(1/5)
+        m = 0.6
+        nu_d = c1 * re_d_max ** m * pr_h ** 0.36 * (pr_h / pr_hs) ** 0.25
+    elif 10**3 < re_d_max <= 2 * 10**5 and 2 < s_t/s_l:
+        c1 = 0.4
+        m = 0.6
+        nu_d = c1 * re_d_max ** m * pr_h ** 0.36 * (pr_h / pr_hs) ** 0.25
+    elif 2 * 10**5 < re_d_max < 2 * 10**6:
+        c1 = 0.022
+        m = 0.84
+        nu_d = c1 * re_d_max ** m * pr_h ** 0.36 * (pr_h / pr_hs) ** 0.25
+    else:
+        Exception("Re_D_max is out of range")
+
+    h_h = ((nu_d * k_h) / tube_diameter_outer).to(ureg.watts / (ureg.meters**2 * ureg.degK))
+
+    return nu_d, h_h
+
+
+def calc_temp_tube(tube_diameter_outer=None, tube_thickness=None, h_h=None, temp_film_c=None,
+                   temp_film_h=None, k_copper_guess=None):
+    # Compare to estimate from above function for Pr_s
+    # Use guess temp value to eval k_copper
+
+    inner_diam = tube_diameter_outer - 2 * tube_thickness
+    r1 = 0.5 * inner_diam.to(ureg.meters)
+    r2 = 0.5 * tube_diameter_outer.to(ureg.meters)
+    h_h_value = Q_(h_h, ureg.watts / (ureg.meters**2 * ureg.degK))
+
+    temp = (k_copper_guess * temp_film_c + h_h_value * r2 * temp_film_h * log(r2/r1)) /\
+                (k_copper_guess + h_h_value * r2 * log(r2/r1))
+    temp_tube = temp.to('degK')
+
+    return temp_tube
+
+
+def calc_hh_less_20_NL(props=None, nu_20_plus_NL=None, N_L=None, tube_diameter_outer=None):
+    k_h = props['k_h']
+
+    c2_values = {
+        '1': 0.64,
+        '2': 0.76,
+        '3': 0.84,
+        '4': 0.89,
+        '5': 0.92,
+        '6': 0.92,
+        '7': 0.95,
+        '8': 0.95,
+        '9': 0.95,
+        '10': 0.97,
+        '11': 0.97,
+        '12': 0.97,
+        '13': 0.98,
+        '14': 0.98,
+        '15': 0.98,
+        '16': 0.99,
+        '17': 0.99,
+        '18': 0.99,
+        '19': 0.99
+    }
+    nu_d = nu_20_plus_NL * c2_values[str(N_L)]
+    h_h = (nu_d * k_h / tube_diameter_outer).to(ureg.watts / (ureg.meters**2 * ureg.degK))
+    return h_h
 
 
 # TODO: Remember to include number of tubes in area calculations
-def calc_length_parametric():
-    return None
+def calc_length(copper_props=None, UA=None, h_c=None, h_h=None, tube_diameter_outer=None, tube_thickness=None):
+    # Uses overall HT coefficient equations
+    inner_diam = tube_diameter_outer - 2 * tube_thickness
+
+    k_copper = copper_props["k_copper"]
+    r2 = tube_diameter_outer.to(ureg.meters) * 0.5
+    r1 = inner_diam.to(ureg.meters) * 0.5
+    h_c = Q_(h_c, ureg.watts / (ureg.meters**2 * ureg.degK))
+    h_h = Q_(h_h, ureg.watts / (ureg.meters**2 * ureg.degK))
+
+
+    l = UA * (1/(h_c * 2 * pi * r1) + log(r2/r1)/(2 * pi * k_copper) + 1/(h_h * 2 * pi * r2))
+    length = l.to(ureg.meters)
+    return length
--- main.py
+++ main.py
@@ -3,22 +3,290 @@ Project 2
 """
 
 from __init__ import ureg, Q_
+import cf
+import numpy as np
 
 
 def main():
-    # Constants
+    # Define Constants
     constants = {
         "temp_amb": 273 * ureg.degK,
         "temp_ic": 293 * ureg.degK,
         "temp_oc": 313 * ureg.degK,
-        "temp_ih": (474 + 273) * ureg.degK,
-        "mdot_h": 1736.4 * (ureg.meters**3 / ureg.seconds),
-        "mdot_c": (1 * ureg.kg / ureg.seconds)  # TODO: Modify if necessary
+        "temp_ih": 747 * ureg.degK,
+        "air_vel": 3000 * ureg.meters/ureg.seconds,   # TODO: Modify if necessary
+        "mdot_h": 1736.4 * (ureg.kg / ureg.seconds),
+        "mdot_c": (1700 * ureg.kg / ureg.seconds),  # TODO: Modify if necessary
+        "s_d": 50 * ureg.mm,
+        "s_l": 40 * ureg.mm,
+        "s_t": 40 * ureg.mm
                  }
 
-    # Parametric variables TODO: select values
-    tube_diameter_outer = [5, 10, 15] * ureg.mm
-    tube_thickness = [0.5, 0.75, 1] * ureg.mm
+    # Define Parametric variables
+    tube_diameter_outer = [Q_(5, 'mm'), Q_(10, 'mm'), Q_(15, 'mm')]
+    tube_thickness = [Q_(0.5, 'mm'), Q_(0.75, 'mm'), Q_(1, 'mm')]
 
-    # Properties TODO: Add properties here
-    props = {}
+    # Properties
+    air_props_temp_inlet_r440a = {
+        # Air properties @ inlet temp, 747K
+        "rho_h": 0.4643 * (ureg.kg / ureg.meters ** 3),
+        "cp_h": 1087 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_h": 354.6 * 10 ** (-7) * (ureg.pascals * ureg.seconds),
+        "nu_h": 76.37 * 10 ** (-6) * (ureg.meters ** 2 / ureg.seconds),
+        "pr_h": 0.702,
+
+        # Fluid (liquid) 1 => R440A @ 303K
+        "temp_sat": 226.5 * ureg.degK,
+        "rho_c": 897.6 * (ureg.kg / ureg.meters ** 3),
+        "cp_c": 1801.1 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_c": 1.6173 * 10 ** (-4) * (ureg.pascals * ureg.seconds),
+        "nu_c": 1.8018 * 10 ** (-7) * (ureg.meters ** 2 / ureg.seconds),
+        "pr_c": 2.986,
+        "k_c": 0.0975 * (ureg.watts / (ureg.meters * ureg.degK))
+    }
+
+    air_props_temp_inlet_r410a = {
+        # Air properties @ inlet temp, 747K
+        "rho_h": 0.4643 * (ureg.kg / ureg.meters ** 3),
+        "cp_h": 1087 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_h": 354.6 * 10 ** (-7) * (ureg.pascals * ureg.seconds),
+        "nu_h": 76.37 * 10 ** (-6) * (ureg.meters ** 2 / ureg.seconds),
+        "k_h": 53.65 * 10 ** (-3) * (ureg.watts / (ureg.meters * ureg.degK)),
+        "pr_h": 0.702,
+
+        # Fluid (liquid) 2 => R410A @ 303K
+        "temp_sat": 221.8 * ureg.degK,
+        "rho_c": 1058 * (ureg.kg / ureg.meters ** 3),
+        "cp_c": 1708.1 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_c": 1.1798 * 10 ** (-4) * (ureg.pascals * ureg.seconds),
+        "nu_c": 1.1144 * 10 ** (-7) * (ureg.meters ** 2 / ureg.seconds),
+        "pr_c": 2.264,
+        "k_c": 0.089 * (ureg.watts / (ureg.meters * ureg.degK))
+    }
+
+    """
+    Step 1: Find temp_film using air props at inlet temp
+    """
+
+    temp_oh_inlet_props_r440a = cf.calc_q_temp_oh(constants=constants, props=air_props_temp_inlet_r440a)[1]
+    temp_oh_inlet_props_r410a = cf.calc_q_temp_oh(constants=constants, props=air_props_temp_inlet_r410a)[1]
+
+    # Film temps
+    temp_film_h = 730.5 * ureg.degK
+    temp_film_c_r410a = 0.5 * (constants['temp_ic'] + constants['temp_oc'])
+    temp_film_c_r440a = 0.5 * (constants['temp_ic'] + constants['temp_oc'])
+
+    # Properties updated (temp_film = 730.5K)
+    r440a_props_temp_film = {
+        # Air properties @ film temp
+        "rho_h": 0.4809 * (ureg.kg / ureg.meters ** 3),
+        "cp_h": 1081 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_h": 346.7 * 10 ** (-7) * (ureg.pascals * ureg.seconds),
+        "nu_h": 72.24 * 10 ** (-6) * (ureg.meters ** 2 / ureg.seconds),
+        "k_h": 53.65 * 10**(-3) * (ureg.watts/(ureg.meters * ureg.degK)),
+        "pr_h": 0.699,
+        "pr_hs": 0.707,     # Note that Pr_s is evaluated at 305K
+
+        # Fluid (liquid) 1 => R440A @ 303K
+        "temp_sat": 226.5 * ureg.degK,
+        "rho_c": 897.6 * (ureg.kg / ureg.meters ** 3),
+        "cp_c": 1801.1 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_c": 1.6173 * 10 ** (-4) * (ureg.pascals * ureg.seconds),
+        "nu_c": 1.8018 * 10 ** (-7) * (ureg.meters ** 2 / ureg.seconds),
+        "pr_c": 2.986,
+        "k_c": 0.0975 * (ureg.watts / (ureg.meters * ureg.degK))
+    }
+
+    # Properties updated (temp_film = 731.6K)
+    r410a_props_temp_film = {
+        # Air properties @ film temp
+        "rho_h": 0.4809 * (ureg.kg / ureg.meters ** 3),
+        "cp_h": 1081 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_h": 346.7 * 10 ** (-7) * (ureg.pascals * ureg.seconds),
+        "nu_h": 72.24 * 10 ** (-6) * (ureg.meters ** 2 / ureg.seconds),
+        "k_h": 53.65 * 10 ** (-3) * (ureg.watts / (ureg.meters * ureg.degK)),
+        "pr_h": 0.699,
+        "pr_hs": 0.707,  # Note that Pr_s is evaluated at 730K
+
+        # Fluid (liquid) 2 => R410A @ 303K
+        "temp_sat": 221.8 * ureg.degK,
+        "rho_c": 1058 * (ureg.kg / ureg.meters ** 3),
+        "cp_c": 1708.1 * (ureg.joules / (ureg.kg * ureg.degK)),
+        "mu_c": 1.1798 * 10 ** (-4) * (ureg.pascals * ureg.seconds),
+        "nu_c": 1.1144 * 10 ** (-7) * (ureg.meters ** 2 / ureg.seconds),
+        "pr_c": 2.264,
+        "k_c": 0.089 * (ureg.watts / (ureg.meters * ureg.degK))
+    }
+
+    """
+    Step 2: Non-parametric calcs - Find UA value
+    """
+
+    # R440A
+    q_r440a, temp_oh_r440a = cf.calc_q_temp_oh(constants=constants, props=r440a_props_temp_film)
+    c_r_r440a, c_min_r440a, c_max_r440a = cf.calc_cr_cmin_cmax(constants=constants, props=r440a_props_temp_film)
+    eta_r440a = cf.calc_eta(constants=constants, c_min=c_min_r440a, q=q_r440a)
+    UA_r440a = cf.calc_ntu(eta=eta_r440a, c_r=c_r_r440a, c_min=c_min_r440a)
+
+    # R410A
+    q_r410a, temp_oh_r410a = cf.calc_q_temp_oh(constants=constants, props=r410a_props_temp_film)
+    c_r_r410a, c_min_r410a, c_max_r410a = cf.calc_cr_cmin_cmax(constants=constants, props=r410a_props_temp_film)
+    eta_r410a = cf.calc_eta(constants=constants, c_min=c_min_r410a, q=q_r410a)
+    UA_r410a = cf.calc_ntu(eta=eta_r410a, c_r=c_r_r410a, c_min=c_min_r410a)
+
+    """
+    Step 3: Parametric calcs - find shell-side and tube-side convection coefficients
+    """
+
+    dimensions_mm = np.full(shape=[3, 3], dtype=list, fill_value=0)
+
+    h_c_r440a = np.full(shape=[3, 3], fill_value=0)
+    re_d_max_r440a = np.full(shape=[3, 3], fill_value=0)
+    nu_20_plus_tubes_r440a = np.full(shape=[3, 3], fill_value=0)
+    h_h_20_plus_tubes_r440a = np.full(shape=[3, 3], fill_value=0)
+    h_h_less_20_tubes_r440a = np.full(shape=[3, 3], fill_value=0)
+
+    h_c_r410a = np.full(shape=[3, 3], fill_value=0)
+    re_d_max_r410a = np.full(shape=[3, 3], fill_value=0)
+    nu_20_plus_tubes_r410a = np.full(shape=[3, 3], fill_value=0)
+    h_h_20_plus_tubes_r410a = np.full(shape=[3, 3], fill_value=0)
+    h_h_less_20_tubes_r410a = np.full(shape=[3, 3], fill_value=0)
+
+    temp_copper_tube_20_plus_NL_r410a = np.full(shape=[3, 3], fill_value=0)
+    temp_copper_tube_less_20_NL_r410a = np.full(shape=[3, 3], fill_value=0)
+    temp_copper_tube_20_plus_NL_r440a = np.full(shape=[3, 3], fill_value=0)
+    temp_copper_tube_less_20_NL_r440a = np.full(shape=[3, 3], fill_value=0)
+
+    length_total_20_plus_tubes_r410a = np.full(shape=[3, 3], fill_value=0)
+    length_total_less_20_tubes_r410a = np.full(shape=[3, 3], fill_value=0)
+    length_total_20_plus_tubes_r440a = np.full(shape=[3, 3], fill_value=0)
+    length_total_less_20_tubes_r440a = np.full(shape=[3, 3], fill_value=0)
+
+    for i, outer_diam in enumerate(tube_diameter_outer):
+        for j, thickness in enumerate(tube_thickness):
+            st = [outer_diam.magnitude, thickness.magnitude]
+            dimensions_mm[i, j] = st
+
+            # R440A
+            h_c_r440a[i, j] = cf.calc_hc(constants=constants, props=r440a_props_temp_film, tube_diameter_outer=outer_diam,
+                                   tube_thickness=thickness).magnitude
+            re_d_max_r440a[i, j] = cf.calc_re_d_max(constants=constants, props=r440a_props_temp_film, tube_diameter_outer=outer_diam)
+
+            nu_20_plus_tubes_r440a[i, j] = cf.calc_nu_hh_20_plus_NL(constants=constants, props=r440a_props_temp_film,
+                                                                                       re_d_max=re_d_max_r440a[i, j],
+                                                                                       tube_diameter_outer=outer_diam)[0]
+            h_h_20_plus_tubes_r440a[i, j] = cf.calc_nu_hh_20_plus_NL(constants=constants, props=r440a_props_temp_film,
+                                                                                       re_d_max=re_d_max_r440a[i, j],
+                                                                                       tube_diameter_outer=outer_diam)[1].magnitude
+            h_h_less_20_tubes_r440a[i, j] = cf.calc_hh_less_20_NL(props=r440a_props_temp_film, nu_20_plus_NL=nu_20_plus_tubes_r440a[i, j],
+                                                            tube_diameter_outer=outer_diam, N_L=15).magnitude   # TODO: N_L is placeholder
+
+            # R410A
+            h_c_r410a[i, j] = cf.calc_hc(constants=constants, props=r410a_props_temp_film, tube_diameter_outer=outer_diam,
+                                   tube_thickness=thickness).magnitude
+
+            re_d_max_r410a[i, j] = cf.calc_re_d_max(constants=constants, props=r410a_props_temp_film, tube_diameter_outer=outer_diam)
+
+            nu_20_plus_tubes_r410a[i, j] = cf.calc_nu_hh_20_plus_NL(constants=constants, props=r410a_props_temp_film,
+                                                                    re_d_max=re_d_max_r410a[i, j],
+                                                                    tube_diameter_outer=outer_diam)[0]
+            h_h_20_plus_tubes_r410a[i, j] = cf.calc_nu_hh_20_plus_NL(constants=constants, props=r410a_props_temp_film,
+                                                                     re_d_max=re_d_max_r410a[i, j],
+                                                                     tube_diameter_outer=outer_diam)[1].magnitude
+
+            h_h_less_20_tubes_r410a[i, j] = cf.calc_hh_less_20_NL(props=r410a_props_temp_film, nu_20_plus_NL=nu_20_plus_tubes_r410a[i, j],
+                                                            tube_diameter_outer=outer_diam, N_L=15).magnitude  # TODO: N_L is placeholder
+
+            # Calc T_s of tube for copper props
+            temp_copper_guess = 305 * ureg.degK
+            k_copper_guess = 401 * (ureg.watts / (ureg.meters * ureg.degK))
+
+            temp_copper_tube_20_plus_NL_r410a[i, j] = cf.calc_temp_tube(tube_diameter_outer=outer_diam, tube_thickness=thickness,
+                                                                  h_h=h_h_20_plus_tubes_r410a[i, j], temp_film_c=temp_film_c_r410a,
+                                                                  temp_film_h=temp_film_h, k_copper_guess=k_copper_guess).magnitude
+            temp_copper_tube_less_20_NL_r410a[i, j] = cf.calc_temp_tube(tube_diameter_outer=outer_diam, tube_thickness=thickness,
+                                                                  h_h=h_h_less_20_tubes_r410a[i, j], temp_film_c=temp_film_c_r410a,
+                                                                  temp_film_h=temp_film_h, k_copper_guess=k_copper_guess).magnitude
+
+            temp_copper_tube_20_plus_NL_r440a[i, j] = cf.calc_temp_tube(tube_diameter_outer=outer_diam, tube_thickness=thickness,
+                                                                  h_h=h_h_20_plus_tubes_r440a[i, j], temp_film_c=temp_film_c_r440a,
+                                                                  temp_film_h=temp_film_h, k_copper_guess=k_copper_guess).magnitude
+            temp_copper_tube_less_20_NL_r440a[i, j] = cf.calc_temp_tube(tube_diameter_outer=outer_diam, tube_thickness=thickness,
+                                                                  h_h=h_h_less_20_tubes_r440a[i, j], temp_film_c=temp_film_c_r440a,
+                                                                  temp_film_h=temp_film_h, k_copper_guess=k_copper_guess).magnitude
+
+            copper_props_305k = {
+                "rho_copper": 8933 * ureg.kg / ureg.meters**3,
+                "cp_copper": 385 * ureg.joules / (ureg.kg * ureg.degK),
+                "k_copper": 401 * (ureg.watts / (ureg.meters * ureg.degK))
+            }
+
+            # Calc HX length
+            length_total_20_plus_tubes_r410a[i, j] = cf.calc_length(copper_props=copper_props_305k, UA=UA_r410a, h_c=h_c_r410a[i, j],
+                                                              h_h=h_h_20_plus_tubes_r410a[i, j], tube_diameter_outer=outer_diam,
+                                                              tube_thickness=thickness).magnitude
+            length_total_less_20_tubes_r410a[i, j] = cf.calc_length(copper_props=copper_props_305k, UA=UA_r410a, h_c=h_c_r410a[i, j],
+                                                              h_h=h_h_less_20_tubes_r410a[i, j], tube_diameter_outer=outer_diam,
+                                                              tube_thickness=thickness).magnitude
+            length_total_20_plus_tubes_r440a[i, j] = cf.calc_length(copper_props=copper_props_305k, UA=UA_r440a, h_c=h_c_r440a[i, j],
+                                                              h_h=h_h_20_plus_tubes_r440a[i, j], tube_diameter_outer=outer_diam,
+                                                              tube_thickness=thickness).magnitude
+            length_total_less_20_tubes_r440a[i, j] = cf.calc_length(copper_props=copper_props_305k, UA=UA_r440a, h_c=h_c_r440a[i, j],
+                                                              h_h=h_h_less_20_tubes_r440a[i, j], tube_diameter_outer=outer_diam,
+                                                              tube_thickness=thickness).magnitude
+
+    print("Outer diameter, Thickness (mm)")
+    print("...")
+    print(dimensions_mm)
+
+    print("R440A - Convection Coefficient (W/m2K)")
+    print("...")
+    print(h_c_r440a)
+
+    print("Exhaust - Convection Coefficient, 15 tubes (W/m2K)")
+    print("...")
+    print(h_h_less_20_tubes_r440a)
+
+    print("Exhaust - Convection Coefficient, >20 tubes (W/m2K)")
+    print("...")
+    print(h_h_20_plus_tubes_r440a)
+
+    print("R440A - Copper Tube Temperature (K)")
+    print("...")
+    print(temp_copper_tube_less_20_NL_r440a)
+
+    print("R440A - HX length (Nt = 15)")
+    print("...")
+    print(length_total_less_20_tubes_r440a)
+
+    print("R440A - HX length (Nt > 20)")
+    print("...")
+    print(length_total_20_plus_tubes_r440a)
+
+    print("R410A - Convection Coefficient (W/m2K)")
+    print("...")
+    print(h_c_r410a)
+
+    print("Exhaust - Convection Coefficient, 15 tubes (W/m2K)")
+    print("...")
+    print(h_h_less_20_tubes_r410a)
+
+    print("Exhaust - Convection Coefficient, >20 tubes (W/m2K)")
+    print("...")
+    print(h_h_20_plus_tubes_r410a)
+
+    print("R410A - Copper Tube Temperature (K)")
+    print("...")
+    print(temp_copper_tube_less_20_NL_r410a)
+
+    print("R410A - HX length (Nt = 15)")
+    print("...")
+    print(length_total_less_20_tubes_r410a)
+
+    print("R410A - HX length (Nt > 20)")
+    print("...")
+    print(length_total_20_plus_tubes_r410a)
+
+
+if __name__ == "__main__":
+    main()
